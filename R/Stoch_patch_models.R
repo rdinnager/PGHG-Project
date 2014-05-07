fd1584c9d5948090d3d1a8000ac299623b3095ca
rdinnager <r.dinnage@gmail.com>
2014-05-07 12:21:44 +0800

    Minor change

--- R/Stoch_patch_models.R
+++ R/Stoch_patch_models.R
@@ -2,10 +2,10 @@ stoch_patch<-function(t,y,params) {
   empt<-y==0 #determine empty patches, 0 means empty, any other y means species with ID number 'y'
   #Add new mortality to empty patch vector; d=death rate
   dead<-c(sample(which(!empt),rbinom(1,sum(!empt),params$d)),which(empt)) 
-  specnums<-table(y) #count how many there are of each species
+  specnums<-table(y[!empt]) #count how many there are of each species
   specprobs2<-rep(0,params$nspec) #initialize species probability vector
   nam<-as.numeric(names(specnums)) #grab the ID numbers of species
-  if (0 %in% nam){nums<-specnums[-which(nam==0)]}else{nums<-specnums} #Get rid of empty cells
+  #if (0 %in% nam){nums<-specnums[-which(nam==0)]}else{nums<-specnums} #Get rid of empty cells
   specprobs2[as.numeric(names(nums))]<-nums/sum(nums) #Species prob is equal to proportion of cells occupied
   #add equal probability of being drawn from species pool. poolprob = prob of immigrant coming from pool
   specprobs <- (1-params$poolprob)*specprobs2+params$poolprob*(1/params$nspec) 
